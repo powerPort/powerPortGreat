a2d44674499c66d55fe9ed79056c5eb8836d8bed
RBK30 <rbk30@RBK30s-Mac-mini.local>
2017-09-30 12:47:07 +0300

    editing the server ,added .DS_store to git ignore file and edit routes functions

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var app = express();
 //the idea i'm trying to do is to get any function from the routes and send the request and the response to it with a callback that will be invoked by that function 
 app.get('/', function (req, res) {
     routes.getMainPage(req, res, function (data) {
-        res.send(data);
+        res.sendFile(data);
     })
 })
-app.get('/someOtherEndPoint', function (req, res) {
-    routes.anotherFunction(req, res, function (data) {
-        res.send(data);
+app.post('/', function (req, res) {
+    routes.findCities(req, res, function (data) {
+        res.sendFile(data);
     })
 })
 
