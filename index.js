45aa8ca00b681717e9691864c56766c2e08f9050
RBK30 <rbk30@RBK30s-Mac-mini.local>
2017-09-30 13:40:45 +0300

    me

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 //main server file
 var express = require('express');
+var bodyParser = require('body-parser');
 var routes = require('./server/routes.js');
 var app = express();
-//app.use(require('body-parser'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 //the idea i'm trying to do is to get any function from the routes and send the request and the response to it with a callback that will be invoked by that function 
 app.get('/', function (req, res) {
     routes.getMainPage(req, res, function (data) {
-        res.sendFile(data);
+        res.send(data);
     })
 })
 app.post('/', function (req, res) {
--- server/routes.js
+++ server/routes.js
@@ -1,23 +1,27 @@
 //this will have to deal with the database 
 var cities = require('../database/index.js');
+const fs = require('fs');
+const path = require('path');
 
 
 exports.getMainPage = function(req, res, callback) {
   //do something to get the data => the main page ;
-  var mainPage = fs.readFileSync('../client/index.html').toString();
-
+  var mainPage = fs.readFileSync(__dirname + '/../client/index.html').toString();
   //test data :
   //var data = 'the main page';
 
   //send it to the callback ;
-  callback(data);
+  callback(mainPage);
 }
 
 
 exports.findCities = function (req, res, CB) {
 	var criteria = req.body;
-	console.log('post req : findCities , criteria : ', criteria);
-  //do something to get results page ..
-
+	console.log('post req : findCities , criteria : ', criteria, req.headers);
+  //do something to get results from db ..
+  var results = cities.find().exec(function(citiesArr) {
+    //do something to the arr
   CB('some data to be sent to the response');
+  })
+
 }
