74682d5c9b7d8eaea22c2ad299d6450aef4f4b36
AlaaRBK <alshyoukhi94@gmail.com>
2017-09-30 13:35:57 +0300

    front-end

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 //main server file
 var express = require('express');
 var routes = require('./server/routes.js');
+var bodyParser = require('body-parser');
 var app = express();
-//app.use(require('body-parser'));
+
+app.use(bodyParser.json());
+
 
 //the idea i'm trying to do is to get any function from the routes and send the request and the response to it with a callback that will be invoked by that function 
 app.get('/', function (req, res) {
-    routes.getMainPage(req, res, function (data) {
-        res.send(data);
-    })
+    console.log(req.body.cost)
 })
 app.get('/someOtherEndPoint', function (req, res) {
     routes.anotherFunction(req, res, function (data) {
