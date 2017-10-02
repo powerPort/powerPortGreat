dfa93246c4fda99e95e6c5ca1748c6e6b077b27a
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-10-02 15:52:32 +0300

    cityinfo with google maps

--- client/componant/cities.js
+++ client/componant/cities.js
@@ -1,12 +1,8 @@
 //angular main component i.e. App
 angular.module('app')
    .component('cities' , {
-   	
 	bindings:{
 		cities : '<'
-	},	
-
+	},
    	templateUrl :`../templates/cities.html`
-   	
-
    })
