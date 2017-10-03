4f8523c80069801b5aadf083403c1a79103f8651
hibaAkroush <hiba.akr@gmail.com>
2017-10-03 15:58:52 +0300

    fetcher and discription

--- server/helpers.js
+++ server/helpers.js
@@ -40,12 +40,12 @@ exports.fetcher = function () {
 	return array;
 }
 
-exports.findDescrption(req,res,callback){
-
+exports.findDescrption = function(req,res,callback){
+	console.log(req.body)
   var city = req.body.name;
+  	console.log("city from post : " ,city)
   var discrption;
-  var url = " https://en.wikipedia.org/w/api.php?action=opensearch&search=" + city +"&limit=100&format=json"
-  
+  var url = " https://en.wikipedia.org/w/api.php?action=opensearch&search=" + city +"&limit=100&format=json"  
   var options = {
 	    url:url ,
 	    headers: {
@@ -65,7 +65,30 @@ exports.findDescrption(req,res,callback){
 	});
 }
 
-
+exports.findImages = function(req,res,callback){
+	var long = req.body
+	var lat = req.body
+	console.log("long and lat from post : ",long,lat)
+	
+	var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + "14228e53e54ac43631ad367e18a534f7" + "&tags=tourism&privacy_filter=1&accuracy=11&safe_search=1&content_type=1&media=photos&lat="+lat +"&lon="+ long +"&per_page=10&format=json&nojsoncallback=1&auth_token=72157687403979384-0c46ae5e0ec4702a&api_sig=9b6e96ef6c1716274952c768b1d685be"
+  var options = {
+	    url:url ,
+	    headers: {
+	      'User-Agent': 'request'
+	    },
+	    method : 'get'
+	}
+	request(url, function (error, response, body) {
+		if (error) {
+		  console.log('error : ', error.message);
+		} else {
+		  body = JSON.parse(body);
+		  console.log(body)	
+		  arrayImages = []
+          callback(arrayImages);
+		}
+	});
+}
 //to switch the key for the api so the api will not block us : we have 60 req allowed per min
 var count = 0;
 exports.API = function (cityName, callback) {
