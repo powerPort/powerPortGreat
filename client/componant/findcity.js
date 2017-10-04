f775645a9a60a80dc0f9618ebd5b5099728fc6a0
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-10-04 12:28:57 +0300

    f

---
+++ client/componant/findcity.js
@@ -0,0 +1,28 @@
+angular.module('app')
+   .component('findcity' , {
+	controller : ($scope, $location) => {
+		this.find = () => {
+			var city = $('#cityName').val()
+			$.ajax({ 
+				type : 'POST',
+				url : 'http://127.0.0.1:3000/cities' ,
+				data :  city,
+				success : function(data) {
+					currentCity = data;
+					$location.path(view);
+					appendMap(); 
+				}
+			}) 
+		}
+	},
+   	templateUrl : `../templates/findcity.html`
+   })
+
+
+
+
+
+
+
+
+
