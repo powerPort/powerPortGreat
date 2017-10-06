0e7425b374eb47f77530fa4c356cdd2fc14f6436
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-10-06 19:20:56 +0300

    F

--- client/map.js
+++ client/map.js
@@ -1,3 +1,4 @@
 function appendMap () {
+	$('#map').text('');
 	$("#map").append('<iframe width="300" height="170" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+currentCity.latitude+','+currentCity.longitude + '&hl=es;z=14&amp;output=embed" ></iframe>')	 
 }
